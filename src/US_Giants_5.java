996df98f280add5c437dad58131e97c18918e350
edmondxiexie <xiejy36@gmail.com>
2016-10-30 00:26:33 -0400

    added 184

--- src/US_Giants_5.java
+++ src/US_Giants_5.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 
 /**
  * Created by Edmond on 10/28/16.
@@ -52,5 +54,49 @@ public class US_Giants_5 {
         } else {
             return -1;
         }
+    }
+
+    /**
+     * 184. Largest Number.
+     * @param num: A list of non negative integers
+     * @return A string
+     */
+    public static String largestNumber(int[] num) {
+        String[] strs = new String[num.length];
+        for (int i = 0; i < num.length; i++) {
+            strs[i] = Integer.toString(num[i]);
+        }
+        Arrays.sort(strs, new strComparator());
+        String result = "";
+        for (String str : strs) {
+            result += str;
+        }
+        System.out.println(result);
+        int index = 0;
+        while (index < result.length()) {
+            if (result.charAt(index) != '0') {
+                break;
+            }
+            index++;
+        }
+        if (index == result.length()) {
+            return "0";
+        }
+        return result.substring(index, result.length());
+    }
+
+    public static class strComparator implements Comparator {
+
+        @Override
+        public int compare(Object o1, Object o2) {
+            String a = (String) o1 + o2;
+            String b = (String) o2 + o1;
+            return -(a.compareTo(b));
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] a = {1,20,23,4,8};
+        System.out.println(largestNumber(a));
     }
 }
