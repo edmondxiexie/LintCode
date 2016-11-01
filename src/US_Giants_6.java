ccfa43281dd51e5ced99a078e4be0adbd1685b2b
edmondxiexie <xiejy36@gmail.com>
2016-10-31 22:33:42 -0400

    added 106

--- src/US_Giants_6.java
+++ src/US_Giants_6.java
@@ -57,12 +57,23 @@ public class US_Giants_6 {
     }
 
     /**
+     * 106. Convert Sorted List to Balanced BST.
      * @param head: The first node of linked list.
      * @return a tree node
      */
     public TreeNode sortedListToBST(ListNode head) {
 
         // write your code here
+    }
+
+    private ListNode getMidNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
     }
 
     public static void main(String[] args) {
